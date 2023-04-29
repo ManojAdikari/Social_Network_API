ecf2f6f54097e7fdfc18a6be8c937e8b0f6ad132
Manoj Adikari <manojpadikari@gmail.com>
2023-04-30 09:15:04 +0930

    Update Servrr.js

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// middleware
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//API routes
 app.use(routes);
 
 db.once("open", () => {
